e26103206e5cb72c8a0d6e87b97cfc99c4ee6267
Valentine <valentinemaillard1@gmail.com>
2020-04-20 12:26:11 -0400

    created basic routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,20 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((request, response) => {
-    response.end("Hello from the server!")
+    const pathName = request.url
+
+    if(pathName === '/' || pathName === '/overview'){
+        response.end("Hello from Overview!")
+    }else if(pathName === '/products'){
+        response.end("Hello from the Products!")
+    }else{
+        response.writeHead(404, {
+            'Contente-Type': 'text/html'
+        },
+            response.end("<h1>Page not found!</h1>")
+        )
+    }
 })
 
 server.listen(3001, 'localhost', () => {
