da29de54791bcc9171b0023e8da53c157cbe6845
Valentine <valentinemaillard1@gmail.com>
2020-04-20 12:49:28 -0400

    created basic api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,14 @@
+const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+
+//////// PARSING DATA INTO JAVASCRIPT ONLY ONCE THE APP IS LAUNCHED ////////
+const data = fs.readFileSync('./dev-data/data.json', 'utf-8')
+const dataObj = JSON.parse(data)
+
+
+//////// SERVER ////////
 const server = http.createServer((request, response) => {
     const pathName = request.url
 
@@ -8,6 +16,12 @@ const server = http.createServer((request, response) => {
         response.end("Hello from Overview!")
     }else if(pathName === '/products'){
         response.end("Hello from the Products!")
+//////// SENDIND RESPONSE BACK FOR API DATA ////////
+    }else if (pathName === '/api'){
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        response.end(data)
     }else{
         response.writeHead(404, {
             'Contente-Type': 'text/html'
