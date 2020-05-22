5f6b122dcc100ececb2198308736facc269debb8
Valentine <valentinemaillard1@gmail.com>
2020-05-22 17:56:35 -0400

    create express server

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+    res.send('You can now post to this endpoint.')
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
