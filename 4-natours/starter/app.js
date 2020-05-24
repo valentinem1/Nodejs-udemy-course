33b9ebd24c9659ebadb20f0eea1b5336b70bd67c
Valentine <valentinemaillard1@gmail.com>
2020-05-24 12:06:07 -0400

    implemented get request for express api

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can now post to this endpoint.')
+// app.post('/', (req, res) => {
+//     res.send('You can now post to this endpoint.')
+// });
+
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+
+app.get('/api/v1/tours', (res, req) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    });
 })
 
 const port = 3000;
