c890565bf7fbda7528ecabb1ec0fb67a83c2bd46
Valentine <valentinemaillard1@gmail.com>
2020-05-26 13:01:19 -0400

    implemented patch request for express api

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -51,6 +51,25 @@ app.post('/api/v1/tours', (req, res) => {
     });
 })
 
+// PATCH request
+app.patch('/api/v1/tours/:id', (req, res) => {
+    // console.log(req.body.name);
+
+    const tour = tours.find(tour => tour.id === parseInt(req.params.id));
+
+    const updatedTour = {...tour, name: req.body.name};
+    const updatedTours = tours.map(tour => tour.id === parseInt(req.params.id) ? updatedTour : tour);
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(updatedTours), err => {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: updatedTour
+            }
+        });
+    });
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
