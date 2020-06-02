b12b9cd70825a42b3bad0cc3f97fb6310adb8635
Valentine <valentinemaillard1@gmail.com>
2020-06-02 12:09:09 -0400

    connected MongoDB to express app with mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,17 @@
 // Must read config file before executing the code in app.js to be able to read the ENV variables
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then( () => console.log("DB connectiong successful! ✅") );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
