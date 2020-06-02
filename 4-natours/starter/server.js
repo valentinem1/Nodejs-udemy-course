172431a5965d172ebe2f66e7ac565e22ac8320eb
Valentine <valentinemaillard1@gmail.com>
2020-06-02 14:09:18 -0400

    created schema example

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -11,7 +11,26 @@ mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-}).then( () => console.log("DB connectiong successful! ✅") );
+})
+.then( () => console.log("DB connectiong successful! ✅") );
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating:{
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
