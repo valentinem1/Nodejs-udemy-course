786516c03f5a4d49c4baad714be3abba84cfb787
Valentine <valentinemaillard1@gmail.com>
2020-06-03 15:29:52 -0400

    implemented get all tours function with mongoose

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,13 +1,21 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        // requestedAt: req.requestTime,
-        // status: 'success',
-        // data: {
-        //     tours
-        // }
-    });
+exports.getAllTours = async (req, res) => {
+    try{
+        const tours = await Tour.find();
+    
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tours
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 };
 
 exports.getTour = (req, res) => {
