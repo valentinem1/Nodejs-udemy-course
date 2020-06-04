e2b32b2898631e07fc0438c505e3a00fb45bb9a1
Valentine <valentinemaillard1@gmail.com>
2020-06-04 12:15:30 -0400

    updated schema

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -4,16 +4,55 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating:{
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a groupe size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage:{
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        // allow white space at the beginnig or end of a string to be cut.
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have an image']
+    },
+    // means type Array of Strings for images
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
