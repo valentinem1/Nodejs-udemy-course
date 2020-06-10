0c3d42966c0b240e98dd9707310d741feff5323d
Valentine <valentinemaillard1@gmail.com>
2020-06-10 16:55:33 -0400

    added query filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,8 +2,25 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try{
-        const tours = await Tour.find();
+        // BUILD THE QUERY
+
+        // spread the query to create a new object that can modify
+        const queryObj = { ...req.query };
+        // save different query types that we then iterate over to delete them from the query url.
+        const excludedFields = ['page', 'sort', 'limit', 'field'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        // console.log(req.query, queryObj);
+
+        // Mongodb query
+        const query = Tour.find(queryObj);
+        // mongoose method to filter the query
+        // const tours = await Tour.find().where('duration').equals(5).where('duration').equals('easy');
     
+        // EXECUTE THE QUERY
+        const tours = await query;
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             data: {
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,9 @@
 // Must read config file before executing the code in app.js to be able to read the ENV variables
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
