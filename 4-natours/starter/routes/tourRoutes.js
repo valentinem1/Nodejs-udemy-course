3ba4d67cea51066402ffa81592dba98e8292823b
Valentine <valentinemaillard1@gmail.com>
2020-06-19 14:58:17 -0400

    added aggregation pipeline for tour stats

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { getAllTours, getTour, createTour, updateTour, deleteTour, topTours } = require('../controllers/tourController')
+const { getAllTours, getTour, createTour, updateTour, deleteTour, topTours, getTourStats } = require('../controllers/tourController')
 const router = express.Router();
 
 // param middleware to check if id is valid before hitting any routes
 // val give us the value of the id
 // router.param('id', checkId);
 
+
 // alias route to get only the top 5, cheapest tours
 router.route('/top-5-tours').get(topTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/')
     .get(getAllTours)
